cd3f414a8ec06f4e6ed5e96110bdba6115c40fd2
primfaith <Robert F Pallante>
2014-05-23 09:47:56 -0400

    Starting to add blocks

--- blockrand.py
+++ blockrand.py
@@ -19,19 +19,32 @@ block_images = []
 for x in blocks:
     block_image=pygame.image.load(x).convert()
     block_images.append(block_image)
-
+pointx=12.5
+pointy=12.5
+flag=0
 ##print block_images
 ####
 while True:
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
 
+    
     choice = randint(0,5)
     print colors[choice]
     bchoice = block_images[choice]
     print bchoice
-    window.blit(bchoice, (300,300))
+    while flag == 0:
+        for x in range (0,4):
+            window.blit(bchoice, bchoice.get_rect(center=(pointx,pointy)))
+            if pointx < 62.5:
+                pointx+=25
+            elif pointx == 62.5:
+                pointy+=25
+            flag+=1
+##    if event .type == KEYDOWN:
+##        if event.key == K_RIGHT:
     pygame.display.update()
     time.sleep(2)
